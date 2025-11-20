97484bc843c322630f098720815574315ce92f85
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2025-11-20 18:45:58 +0530

    jf

--- TODO/todo_frontend/src/Home.js
+++ TODO/todo_frontend/src/Home.js
@@ -43,7 +43,7 @@ const Home = () => {
                 setTodos(updatedTodos);
                 setTaskid('');
                 setUpdatetask('');
-                globalThis.location.reload(); 
+                window.location.reload(); 
             })
             .catch(err => console.log(err));
     };
